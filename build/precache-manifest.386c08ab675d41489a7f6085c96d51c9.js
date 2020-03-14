fd8e4422e8de10cd79aaa9e3fa3fb58406827b6f
agboola.omowumi <agboola.omowumi@konga.com>
2020-03-14 19:34:29 +0100

    Add todo app using react hook

---
+++ build/precache-manifest.386c08ab675d41489a7f6085c96d51c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bd07336cc7767000a3ff9dce61849e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1216e52da106916adb8d",
+    "url": "/static/css/main.0360e680.chunk.css"
+  },
+  {
+    "revision": "49081c0613ec49041777",
+    "url": "/static/js/2.99e3c210.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.99e3c210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1216e52da106916adb8d",
+    "url": "/static/js/main.67e8dc71.chunk.js"
+  },
+  {
+    "revision": "bf5ff23e0477b8e35c10",
+    "url": "/static/js/runtime-main.f81a141c.js"
+  }
+]);
